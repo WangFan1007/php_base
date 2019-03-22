e425c044733fa1e73cb11ff60fe3c98add0b541b
wangfan <1036815077@qq.com>
2019-03-22 16:58:37 +0800

    some change

--- Sql.php
+++ Sql.php
@@ -2,5 +2,45 @@
 
 class Sql
 {
-    
+    public $host;
+    public $port;
+    public $user;
+    public $pass;
+    public $dbname;
+    public $charset;
+
+    public function __construct(array $info = array())
+    {
+        $this->host = $info['host'] ?? 'localhost';
+        $this->port = $info['port'] ?? '3306';
+        $this->user = $info['user'] ?? 'root';
+        $this->pass = $info['pass'] ?? 'root';
+        $this->dbname = $info['dbname'] ?? 'my_database';
+        $this->charset = $info['charset'] ?? 'utf8';
+
+        $this->sql_connect();
+        $this->sql_charset();
+    }
+
+    public $link;
+    public function sql_connect()
+    {
+        $this->link = @new Mysqli($this->host,$this->user,$this->pass,$this->dbname,$this->port);
+        if ($this->link->connect_error) {
+            die('Connect Error(' . $this->link->connect_errno . ')' . $this->link->connect_error);
+        }
+    }
+
+    public function sql_charset()
+    {
+        $sql = "set names {$this->charset}";
+        $res = $this->link->query($sql);
+        if (!$res) {
+            die('Charset Error(' . $this->link->errno . ')' . $this->link->error);
+        }
+    }
 }
+
+$s = new Sql(array('pass'=>'','charset'=>'utf8'));
+// $s->sql_connect();
+// var_dump($s);
