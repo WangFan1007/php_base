d2c40c17f5278bcec9728926de605000e90b6e1d
wangfan <1036815077@qq.com>
2019-04-24 10:20:49 +0800

    php-mogodb

--- mongo/1.php
+++ mongo/1.php
@@ -3,9 +3,16 @@
 // $client = new \MongoDB\Client("mongodb://192.168.8.222:27017");
 require '../vendor/autoload.php';
 
-$client = new MongoDB\Client("mongodb://192.168.8.222:27017");
+//mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+// $client = new MongoDB\Client("mongodb://192.168.8.222:27017/php61");
+$client = new MongoDB\Client("mongodb://192.168.8.222:27017/");
+// $collection = $client->php61->goods;
+// $result = $collection->find();
 $collection = $client->php61->goods;
-$result = $collection->find();
+$cursor = $collection->find([],[]);
 
-vardump($result);
+echo '<pre>';
+foreach ($cursor as $document) {
+    var_dump($document);
+}
 
